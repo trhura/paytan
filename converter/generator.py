5205ffdddc32a6a3584186f05f266c3483da6daf
Thura Hlaing <trhura@gmail.com>
2014-05-18 00:49:40 +0630

    added initial implementation

--- converter/generator.py
+++ converter/generator.py
@@ -2,10 +2,10 @@
 
 import pathlib
 import csv
+from jinja2 import Template
 
 def main():
     cwd_dir = pathlib.Path(__file__).parent
-    child_dirs = [d for d in cwd_dir.iterdir() if d.is_dir()]
     rules_files = list(cwd_dir.glob('*.rules'))
 
     context = {}
@@ -14,9 +14,11 @@ def main():
             rules_reader = csv.reader(rules,delimiter='|')
             strip_column = lambda l: list(map(lambda i: i.strip(), l))
             rules_list = [strip_column(r) for r in rules_reader]
-            context[each_rules_file.stem] = rules_list
+            context[each_rules_file.stem + '_rules'] = rules_list
 
-    #for child_dir in child_dirs:
-        #with
+    for template_file in cwd_dir.glob("*.template"):
+        with template_file.open('r') as templateFile, open(template_file.stem, 'w') as outputFile:
+            template = Template(templateFile.read())
+            outputFile.write(template.render(**context))
 
 main()
