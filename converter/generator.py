4f0af22b937fa74f4eca58b6863b27d943845d1d
Thura Hlaing <trhura@gmail.com>
2014-05-18 09:43:15 +0630

    first working uni512zg1 python

--- converter/generator.py
+++ converter/generator.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python3
 
-import pathlib
 import csv
+import os
+import pathlib
 from jinja2 import Template
 
 def main():
     cwd_dir = pathlib.Path(__file__).parent
     rules_files = list(cwd_dir.glob('*.rules'))
+    child_dirs = [x for x in cwd_dir.iterdir() if x.is_dir()]
 
     context = {}
     for each_rules_file in rules_files:
@@ -16,9 +18,11 @@ def main():
             rules_list = [strip_column(r) for r in rules_reader]
             context[each_rules_file.stem + '_rules'] = rules_list
 
-    for template_file in cwd_dir.glob("*.template"):
-        with template_file.open('r') as templateFile, open(template_file.stem, 'w') as outputFile:
-            template = Template(templateFile.read())
-            outputFile.write(template.render(**context))
+    for child_dir in child_dirs:
+        for template_file in child_dir.glob("*.template"):
+            with template_file.open('r') as templateFile, \
+                open(os.path.join(child_dir.as_posix(), template_file.stem), 'w') as outputFile:
+                template = Template(templateFile.read())
+                outputFile.write(template.render(**context))
 
 main()
